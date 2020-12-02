4b563bfdfe49d1305ee3f2800b69b86811a82426
Bruno Konrad <bruno.skon@gmail.com>
2020-12-02 20:53:50 +0100

    Improve password specs
    Create subject for the main context and define fixture for each scenario

--- spec/advent_of_code_2020/day2_spec.rb
+++ spec/advent_of_code_2020/day2_spec.rb
@@ -3,69 +3,71 @@ require "advent_of_code_2020/day2"
 
 RSpec.describe AdventOfCode2020::Day2::Password do
   context "when using SledRentalPlaceCorporatePolicy" do
+    subject {
+      subject = described_class.new(
+        fixture,
+        AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy
+      )
+    }
+
     describe "#valid?" do
-      it "returns true if password matches its criteria" do
-        fixture = "1-3 a: abcde"
-        subject = described_class.new(
-          fixture,
-          AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy
-        )
-  
-        expect(subject.valid?).to be(true)
+      context "when fixture is '1-3 a: abcde'" do
+        let(:fixture) { "1-3 a: abcde" }
+
+        it "returns true if password matches its criteria" do
+          expect(subject.valid?).to be(true)
+        end
       end
-  
-      it "returns false if password does not match its criteria" do
-        fixture = "1-3 b: cdefg"
-        subject = described_class.new(
-          fixture,
-          AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy
-        )
-  
-        expect(subject.valid?).to be(false)
+
+      context "when fixture is '1-3 b: cdefg'" do
+        let(:fixture) { "1-3 b: cdefg" }
+
+        it "returns false if password does not match its criteria" do
+          expect(subject.valid?).to be(false)
+        end
       end
-  
-      it "rename me" do
-        fixture = "2-9 c: ccccccccc"
-        subject = described_class.new(
-          fixture,
-          AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy
-        )
-  
-        expect(subject.valid?).to be(true)
+
+      context "when fixture is '2-9 c: ccccccccc'" do
+        let(:fixture) { "2-9 c: ccccccccc" }
+
+        it "rename me" do
+          expect(subject.valid?).to be(true)
+        end
       end
     end
   end
 
   context "when using TobogganCorporatePolicy" do
+    subject {
+      described_class.new(
+        fixture,
+        AdventOfCode2020::Day2::TobogganCorporatePolicy
+      )
+    }
+
     describe "#valid?" do
-      it "returns true if password matches its criteria" do
-        fixture = "1-3 a: abcde"
-        subject = described_class.new(
-          fixture,
-          AdventOfCode2020::Day2::TobogganCorporatePolicy
-        )
-  
-        expect(subject.valid?).to be(true)
+      context "when fixture is '1-3 a: abcde'" do
+        let(:fixture) { "1-3 a: abcde" }
+
+        it "returns true if password matches its criteria" do
+          expect(subject.valid?).to be(true)
+        end
       end
   
-      it "returns false if password does not match its criteria" do
-        fixture = "1-3 b: cdefg"
-        subject = described_class.new(
-          fixture,
-          AdventOfCode2020::Day2::TobogganCorporatePolicy
-        )
-  
-        expect(subject.valid?).to be(false)
+      context "when fixture is '1-3 b: cdefg'" do
+        let(:fixture) { "1-3 a: cdefg" }
+
+        it "returns false if password does not match its criteria" do
+          expect(subject.valid?).to be(false)
+        end
       end
-  
-      it "rename me" do
-        fixture = "2-9 c: ccccccccc"
-        subject = described_class.new(
-          fixture,
-          AdventOfCode2020::Day2::TobogganCorporatePolicy
-        )
-  
-        expect(subject.valid?).to be(false)
+      
+      context "when fixture is '2-9 c: ccccccccc'" do
+        let(:fixture) { "2-9 c: ccccccccc" }
+
+        it "returns false" do
+          expect(subject.valid?).to be(false)
+        end
       end
     end
   end
