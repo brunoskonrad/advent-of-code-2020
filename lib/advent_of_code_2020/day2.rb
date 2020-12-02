776e792f01ff27bbcc73a0ce9998ad6779e82be5
Bruno Konrad <bruno.skon@gmail.com>
2020-12-02 20:45:28 +0100

    Second part, done. all the struggle. it was real

--- lib/advent_of_code_2020/day2.rb
+++ lib/advent_of_code_2020/day2.rb
@@ -3,15 +3,43 @@ module AdventOfCode2020
 
     # 2-4 p: vpkpp
     class Password
-      def initialize(password_entry)
+      def initialize(password_entry, corporate_policy)
         password_parts = password_entry.split(":").map(&:strip)
 
-        @corporate_policy = SledRentalPlaceCorporatePolicy.new(password_parts[0])
+        @corporate_policy = corporate_policy.new(password_parts[0])
         @password = password_parts[1]
       end
 
       def valid?
         @corporate_policy.validate(@password)
+      end
+    end
+
+    class TobogganCorporatePolicy
+      def initialize(policy)
+        parts = policy.split(" ")
+
+        @range = parts[0].split("-").map(&:to_i)
+        @character = parts[1]
+      end
+
+      def validate(password)
+        char1 = password[position_1]
+        char2 = password[position_2]
+
+        return false if char1 == char2
+
+        char1 == @character || char2 == @character
+      end
+
+      private
+
+      def position_1
+        @range[0] - 1
+      end
+
+      def position_2
+        @range[1] - 1
       end
     end
 
@@ -39,7 +67,7 @@ module AdventOfCode2020
         .read
         .split(/\n/)
         .map(&:strip)
-        .map { |password| Password.new(password) }
+        .map { |password| Password.new(password, TobogganCorporatePolicy) }
         .select(&:valid?)
         .count
 
--- spec/advent_of_code_2020/day2_spec.rb
+++ spec/advent_of_code_2020/day2_spec.rb
@@ -2,26 +2,71 @@ require 'spec_helper'
 require "advent_of_code_2020/day2"
 
 RSpec.describe AdventOfCode2020::Day2::Password do
-  describe "#valid?" do
-    it "returns true if password matches its criteria" do
-      fixture = "1-3 a: abcde"
-      subject = described_class.new(fixture)
-
-      expect(subject.valid?).to be(true)
+  context "when using SledRentalPlaceCorporatePolicy" do
+    describe "#valid?" do
+      it "returns true if password matches its criteria" do
+        fixture = "1-3 a: abcde"
+        subject = described_class.new(
+          fixture,
+          AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy
+        )
+  
+        expect(subject.valid?).to be(true)
+      end
+  
+      it "returns false if password does not match its criteria" do
+        fixture = "1-3 b: cdefg"
+        subject = described_class.new(
+          fixture,
+          AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy
+        )
+  
+        expect(subject.valid?).to be(false)
+      end
+  
+      it "rename me" do
+        fixture = "2-9 c: ccccccccc"
+        subject = described_class.new(
+          fixture,
+          AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy
+        )
+  
+        expect(subject.valid?).to be(true)
+      end
     end
+  end
 
-    it "returns false if password does not match its criteria" do
-      fixture = "1-3 b: cdefg"
-      subject = described_class.new(fixture)
-
-      expect(subject.valid?).to be(false)
-    end
-
-    it "rename me" do
-      fixture = "2-9 c: ccccccccc"
-      subject = described_class.new(fixture)
-
-      expect(subject.valid?).to be(true)
+  context "when using TobogganCorporatePolicy" do
+    describe "#valid?" do
+      it "returns true if password matches its criteria" do
+        fixture = "1-3 a: abcde"
+        subject = described_class.new(
+          fixture,
+          AdventOfCode2020::Day2::TobogganCorporatePolicy
+        )
+  
+        expect(subject.valid?).to be(true)
+      end
+  
+      it "returns false if password does not match its criteria" do
+        fixture = "1-3 b: cdefg"
+        subject = described_class.new(
+          fixture,
+          AdventOfCode2020::Day2::TobogganCorporatePolicy
+        )
+  
+        expect(subject.valid?).to be(false)
+      end
+  
+      it "rename me" do
+        fixture = "2-9 c: ccccccccc"
+        subject = described_class.new(
+          fixture,
+          AdventOfCode2020::Day2::TobogganCorporatePolicy
+        )
+  
+        expect(subject.valid?).to be(false)
+      end
     end
   end
 end
@@ -34,10 +79,26 @@ RSpec.describe AdventOfCode2020::Day2::SledRentalPlaceCorporatePolicy do
       expect(subject.validate("abcde")).to be(true)
     end
 
-    it "returns true if given string is valid" do
+    it "returns false if given string is not valid" do
       subject = described_class.new("1-3 a")
 
       expect(subject.validate("defghi")).to be(false)
+    end
+  end
+end
+
+RSpec.describe AdventOfCode2020::Day2::TobogganCorporatePolicy do
+  describe "#validate" do
+    it "returns true if given string is valid" do
+      subject = described_class.new("1-3 a")
+
+      expect(subject.validate("abcde")).to be(true)
+    end
+
+    it "returns false if given string is not valid" do
+      subject = described_class.new("1-3 b")
+
+      expect(subject.validate("cdefg")).to be(false)
     end
   end
 end
