a5ca986b5d2edb02dda15e8347925d819cc5b944
Bruno Konrad <bruno.skon@gmail.com>
2020-12-03 21:30:09 +0100

    First step for day 3

---
+++ lib/advent_of_code_2020/day3.rb
@@ -0,0 +1,83 @@
+module Day3
+
+  class Grid
+    def initialize(input)
+      @input = input.split(/\n/)
+    end
+
+    def point(x, y)
+      return :void if y >= rows
+
+      normalized_x = x % grid_width
+      value = @input[y][normalized_x]
+
+      if value == "#"
+        :tree
+      else
+        :open_square
+      end
+    end
+
+    def rows
+      @input.length
+    end
+
+    private 
+
+    def grid_width
+      @input[0].length
+    end
+  end
+
+  class Toboggan
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x = x
+      @y = y
+    end
+
+    def move
+      @x += 3
+      @y += 1
+    end
+  end
+
+  class World
+    attr_reader :collided_trees_count
+
+    def initialize(grid, vehicle)
+      @grid = grid
+      @vehicle = vehicle
+      @collided_trees_count = 0
+    end
+
+    def tick
+      @vehicle.move
+      tile = @grid.point(@vehicle.x, @vehicle.y)
+
+      @collided_trees_count += 1 if tile == :tree
+    end
+
+    def run
+      while @vehicle.y <= @grid.rows - 1
+        tick
+      end
+    end
+  end
+
+  def self.main
+    input = File.new("input/day3.txt").read
+    world = World.new(
+      Grid.new(input),
+      Toboggan.new(0, 0)
+    )
+
+    world.run
+
+    p world.collided_trees_count
+  end
+
+end
+
+Day3::main()
---
+++ spec/advent_of_code_2020/day3_spec.rb
@@ -0,0 +1,79 @@
+require "advent_of_code_2020/day3"
+
+RSpec.describe Day3::Grid do
+  let(:fixture) {
+    File.new("input/day3_fixture.txt").read
+  }
+
+  subject {
+    described_class.new(fixture)
+  }
+
+  describe "#rows" do
+    it "returns the amount of rows for given grid" do
+      expect(subject.rows).to eq(11)
+    end
+  end
+  
+  describe "#point" do
+    it "returns entity for given vector within grid length" do
+      expect(subject.point(0, 0)).to eq(:open_square)
+      expect(subject.point(0, 1)).to eq(:tree)
+    end
+
+    it "returns entity for given vector outside grid length" do
+      expect(subject.point(11, 0)).to eq(:open_square)
+      expect(subject.point(11, 1)).to eq(:tree)
+
+      expect(subject.point(13, 0)).to eq(:tree)
+      expect(subject.point(24, 0)).to eq(:tree)
+    end
+
+    it "do not let vector outside vertical grid" do
+      expect(subject.point(0, 11)).to eq(:void)
+    end
+  end
+
+end
+
+RSpec.describe Day3::Toboggan do
+  subject {
+    described_class.new(0, 0)
+  }
+
+  describe "#move" do
+    it "moves 3 to the right, 1 down" do
+      subject.move
+
+      expect(subject.x).to eq(3)
+      expect(subject.y).to eq(1)
+
+      subject.move
+
+      expect(subject.x).to eq(6)
+      expect(subject.y).to eq(2)
+    end
+  end
+end
+
+RSpec.describe Day3::World do
+  let(:fixture) {
+    File.new("input/day3_fixture.txt").read
+  }
+
+  subject {
+    described_class.new(
+      Day3::Grid.new(fixture),
+      Day3::Toboggan.new(0, 0)
+    )
+  }
+
+  describe "#run" do
+    it "runs through the grid and count how many collided trees" do
+      subject.run
+
+      expect(subject.collided_trees_count).to eq(7)
+    end
+  end
+
+end
