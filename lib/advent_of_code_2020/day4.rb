28e6231a4348b4c680ebf671d33ac1a90a625c30
Bruno Konrad <bruno.skon@gmail.com>
2020-12-04 20:08:42 +0100

    Part 2

--- lib/advent_of_code_2020/day4.rb
+++ lib/advent_of_code_2020/day4.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Day4
 
   def self.parse_string_input_to_hash(string_input)
@@ -7,6 +9,49 @@ module Day4
 
       acc[temp[0]] = temp[1]
       acc
+    end
+  end
+
+  class Validator
+    EYE_COLOURS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].freeze
+
+    def self.valid_year?(year, valid_range)
+      return false unless year
+
+      valid_range.include?(year.to_i)
+    end
+
+    def self.valid_height?(height)
+      return false unless height
+
+      match = height.match(/(\d{2,3})(in|cm)/i)
+      return false unless match
+
+      entries = match.captures
+
+      if entries[1] == "cm"
+        (150..193).include?(entries[0].to_i)
+      else
+        (59..76).include?(entries[0].to_i)
+      end
+    end
+
+    def self.valid_hex_colour?(colour)
+      return false unless colour
+
+      /^#[a-f0-9]{6}$/i.match?(colour)
+    end
+
+    def self.valid_eye_colour?(eye_colour)
+      return false unless eye_colour
+
+      EYE_COLOURS.include?(eye_colour)
+    end
+
+    def self.valid_passport_number?(passport_number)
+      return false unless passport_number
+
+      /^\d{9}$/i.match?(passport_number)
     end
   end
 
@@ -24,14 +69,43 @@ module Day4
     end
 
     def valid?
-      @document_information.include?("ecl") and
-      @document_information.include?("pid") and
-      @document_information.include?("eyr") and
-      @document_information.include?("hcl") and
-      @document_information.include?("byr") and
-      @document_information.include?("iyr") and
-      @document_information.include?("hgt")
+      valid_byr? and
+      valid_iyr? and
+      valid_eyr? and
+      valid_hgt? and
+      valid_hcl? and
+      valid_ecl? and
+      valid_pid?
     end
+
+    def valid_byr?
+      Validator.valid_year?(@document_information["byr"], 1920..2002)
+    end
+
+    def valid_iyr?
+      Validator.valid_year?(@document_information["iyr"], 2010..2020)
+    end
+
+    def valid_eyr?
+      Validator.valid_year?(@document_information["eyr"], 2020..2030)
+    end
+
+    def valid_hgt?
+      Validator.valid_height?(@document_information["hgt"])
+    end
+
+    def valid_hcl?
+      Validator.valid_hex_colour?(@document_information["hcl"])
+    end
+
+    def valid_ecl?
+      Validator.valid_eye_colour?(@document_information["ecl"])
+    end
+
+    def valid_pid?
+      Validator.valid_passport_number?(@document_information["pid"])
+    end
+
   end
 
   def self.main
--- spec/advent_of_code_2020/day4_spec.rb
+++ spec/advent_of_code_2020/day4_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 require "advent_of_code_2020/day4"
 
+require "pry"
+
 RSpec.describe "loading functions" do
   describe "#parse_string_input_to_hash" do
     it " " do
@@ -16,6 +18,112 @@ RSpec.describe "loading functions" do
         "cid" => "147",
         "hgt" => "183cm",
       })
+    end
+  end
+end
+
+RSpec.describe Day4::Validator do
+  subject {
+    Day4::Validator
+  }
+
+  describe "#valid_year?" do
+    it "returns false when year is not present" do
+      expect(subject.valid_year?(nil, 2000..2010)).to eq(false)
+    end
+
+    it "retuns false when year is outside range" do
+      expect(subject.valid_year?(1990, 2000..2010)).to eq(false)
+      expect(subject.valid_year?(2020, 2000..2010)).to eq(false)
+    end
+
+    it "retuns true when year is inside range" do
+      expect(subject.valid_year?(2000, 2000..2010)).to eq(true)
+      expect(subject.valid_year?(2005, 2000..2010)).to eq(true)
+      expect(subject.valid_year?(2010, 2000..2010)).to eq(true)
+    end
+  end
+
+  describe "#valid_height?" do
+    it "returns false when year is not present" do
+      expect(subject.valid_height?(nil)).to eq(false)
+    end
+
+    it "validates heights in inches" do
+      expect(subject.valid_height?('nope')).to eq(false)
+    end
+
+    it "validates heights in inches" do
+      expect(subject.valid_height?('58in')).to eq(false)
+      expect(subject.valid_height?('77in')).to eq(false)
+
+      expect(subject.valid_height?('59in')).to eq(true)
+      expect(subject.valid_height?('76in')).to eq(true)
+      expect(subject.valid_height?('74in')).to eq(true)
+    end
+
+    it "validates heights in centimeters" do
+      expect(subject.valid_height?('149cm')).to eq(false)
+      expect(subject.valid_height?('194cm')).to eq(false)
+
+      expect(subject.valid_height?('150cm')).to eq(true)
+      expect(subject.valid_height?('193cm')).to eq(true)
+    end
+  end
+
+  describe "#valid_hex_colour?" do
+    it "returns false when year is not present" do
+      expect(subject.valid_hex_colour?(nil)).to eq(false)
+    end
+
+    it "validates hex colour" do
+      expect(subject.valid_hex_colour?('#0000000')).to eq(false)
+      expect(subject.valid_hex_colour?('#00000')).to eq(false)
+      expect(subject.valid_hex_colour?('#ghijlm')).to eq(false)
+
+      expect(subject.valid_hex_colour?('#000000')).to eq(true)
+      expect(subject.valid_hex_colour?('#abcdef')).to eq(true)
+      expect(subject.valid_hex_colour?('#059def')).to eq(true)
+    end
+  end
+  
+  describe "#valid_eye_colour?" do
+    it "returns false when year is not present" do
+      expect(subject.valid_eye_colour?(nil)).to eq(false)
+    end
+
+    it "validates hex colour" do
+      expect(subject.valid_eye_colour?('nope')).to eq(false)
+
+      expect(subject.valid_eye_colour?('amb')).to eq(true)
+      expect(subject.valid_eye_colour?('blu')).to eq(true)
+      expect(subject.valid_eye_colour?('brn')).to eq(true)
+      expect(subject.valid_eye_colour?('gry')).to eq(true)
+      expect(subject.valid_eye_colour?('grn')).to eq(true)
+      expect(subject.valid_eye_colour?('hzl')).to eq(true)
+      expect(subject.valid_eye_colour?('oth')).to eq(true)
+    end
+  end
+
+  describe "#valid_passport_number?" do
+    it "returns false when year is not present" do
+      expect(subject.valid_passport_number?(nil)).to eq(false)
+    end
+
+    it "validates hex colour" do
+      expect(subject.valid_passport_number?('0000000001')).to eq(false)
+      expect(subject.valid_passport_number?('00000001')).to eq(false)
+      expect(subject.valid_passport_number?('1')).to eq(false)
+
+      expect(subject.valid_passport_number?('000000001')).to eq(true)
+      expect(subject.valid_passport_number?('000000011')).to eq(true)
+      expect(subject.valid_passport_number?('000000111')).to eq(true)
+      expect(subject.valid_passport_number?('000001111')).to eq(true)
+      expect(subject.valid_passport_number?('000011111')).to eq(true)
+      expect(subject.valid_passport_number?('000111111')).to eq(true)
+      expect(subject.valid_passport_number?('001111111')).to eq(true)
+      expect(subject.valid_passport_number?('011111111')).to eq(true)
+      expect(subject.valid_passport_number?('111111111')).to eq(true)
     end
   end
 end
@@ -42,41 +150,34 @@ RSpec.describe Day4::LoadPassports do
 end
 
 RSpec.describe Day4::Document do
-
-  subject {
-    described_class.new(document_fixture)
-  }
-
-  describe "#valid?" do
-    context "first fixture set of data" do
-      let(:document_fixture) { "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm" }
   
-      it "returns true" do
-        expect(subject.valid?).to eq(true)
-      end
-    end
+  context "part 2, invalid passports" do
+    let(:invalid_passports) {
+      Day4::LoadPassports.call(
+        File.new("fixtures/day4_invalid_passports.txt").read
+      )
+    }
 
-    context "second fixture set of data" do
-      let(:document_fixture) { "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929" }
-  
-      it "returns true" do
+    it "please work in future" do
+      invalid_passports.each do |document|
+        subject = described_class.new(document)
         expect(subject.valid?).to eq(false)
       end
     end
+  end
 
-    context "third fixture set of data" do
-      let(:document_fixture) { "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm" }
-  
-      it "returns true" do
+  context "part 2, valid passports" do
+    let(:valid_passports) {
+      Day4::LoadPassports.call(
+        File.new("fixtures/day4_valid_passports.txt").read
+      )
+    }
+
+    it "is it working?" do
+      valid_passports.each do |document|
+        subject = described_class.new(document)
+
         expect(subject.valid?).to eq(true)
-      end
-    end
-
-    context "fourth fixture set of data" do
-      let(:document_fixture) { "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in" }
-  
-      it "returns true" do
-        expect(subject.valid?).to eq(false)
       end
     end
   end
