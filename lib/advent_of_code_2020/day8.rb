7b411e3154fd2307572db6d3950fbe0e682e4770
Bruno Konrad <bruno.skon@gmail.com>
2020-12-08 22:34:47 +0100

    Day 8, part 2
    It's not pretty, but it does the trick

--- lib/advent_of_code_2020/day8.rb
+++ lib/advent_of_code_2020/day8.rb
@@ -3,10 +3,10 @@ require "pry"
 module Day8
   
   class BootCode
-    attr_accessor :accumulator
+    attr_reader :accumulator
 
-    def initialize(program)
-      @instructions = Instructions.new(program)
+    def initialize(instructions)
+      @instructions = instructions
       @accumulator = 0
 
       @executed_instructions_lines = []
@@ -21,6 +21,7 @@ module Day8
         @executed_instructions_lines << @current_instruction_line
 
         instruction = @instructions.get_instruction_at_line(@current_instruction_line)
+
         operation = instruction.operation.new(instruction.argument)
 
         @accumulator = operation.call(@accumulator)
@@ -28,10 +29,14 @@ module Day8
       end
     end
 
+    def run_whole_program?
+      @instructions.count - 1 == @current_instruction_line
+    end
+
     private
 
     def can_execute?(line)
-      !@executed_instructions_lines.include?(line)
+      !@executed_instructions_lines.include?(line) && line < @instructions.count
     end
   end
 
@@ -47,6 +52,43 @@ module Day8
 
       Instruction.new(parts[1], OPERATIONS[parts[0]])
     end
+
+    def count
+      @program.count
+    end
+  end
+
+  LineInstruction = Struct.new(:line, :instruction)
+
+  class HijackedInstructions < Instructions
+    
+    attr_accessor :replace_at
+
+    def count_lines_to_change
+      jmp_nop_commands.count
+    end
+
+    def get_instruction_at_line(line)
+      if hijacked_instruction.line == line
+        parts = @program[line].split(" ")
+        hijacked_operation = parts[0] == "jmp" ? "nop" : "jmp"
+
+        Instruction.new(parts[1], OPERATIONS[hijacked_operation])
+      else
+        super
+      end
+    end
+
+    def hijacked_instruction
+      jmp_nop_commands[replace_at]
+    end
+
+    def jmp_nop_commands
+      @jmp_nop_commands ||= @program
+        .map.with_index { |instruction, index| LineInstruction.new(index, instruction) }
+        .filter { |line| /jmp|nop/.match(line.instruction) }
+    end
+
   end
 
   class Operator
@@ -100,14 +142,31 @@ module Day8
   }.freeze
 
   def self.part_one
-    input = File.new("input/day8_part1.txt").read
-    boot_code = BootCode.new(input)
+    instructions = Instructions.new(File.new("input/day8.txt").read)
+    boot_code = BootCode.new(instructions)
 
     boot_code.run
 
-    p boot_code.accumulator
+    puts boot_code.accumulator
+  end
+
+  def self.run_part_two(instructions, index)
+    instructions.replace_at = index
+    boot_code = BootCode.new(instructions)
+
+    boot_code.run
+
+    return boot_code.accumulator if boot_code.run_whole_program?
+    Day8::run_part_two(instructions, index + 1) if instructions.count_lines_to_change > index
+  end
+
+  def self.part_two
+    instructions = HijackedInstructions.new(File.new("input/day8.txt").read)
+    result = Day8::run_part_two(instructions, 0)
+
+    puts result
   end
 
 end
 
-Day8::part_one()
+Day8::part_two()
